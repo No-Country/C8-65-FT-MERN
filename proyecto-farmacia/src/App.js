c5d8d3705969309aa11e385e48abb065db87f4e1
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-11-15 00:36:03 -0300

    Arreglo de tailwind

--- proyecto-farmacia/src/App.js
+++ proyecto-farmacia/src/App.js
@@ -5,27 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div >
-      <NavBar />
-      <h1 className='text-red-600'>hola</h1>
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          holaaa
-        </a>
-      </header>
-
-    </div>
+    <h1 className='text-10xl font-bold text-blue-600 bg-pink-800'>Hola mundo</h1>
   );
 }
 
--- proyecto-farmacia/src/components/NavBar/NavBar.jsx
+++ proyecto-farmacia/src/components/NavBar/NavBar.jsx
@@ -2,26 +2,11 @@ import React, { useState } from 'react'
 
 
 const NavBar = () => {
-    const [color, setcolor] = useState(true)
-    const [palabra, setpalabra] = useState('hola')
-
-    const cambiarColor = () => {
-        setcolor(!color)
-        setpalabra('chau')
-    }
-
-    const palabraFuncion = () => {
-        setpalabra('hola')
-    }
 
     return (
-        <nav>
-            <div className={color ? 'bg-red-700' : 'bg-blue-400'}>NavBar</div>
-            <button onClick={cambiarColor} className='bg-amber-300'>cambio</button>
-            <h2 className='text-red-600'>{palabra}</h2>
-            <button onClick={palabraFuncion}>cambiar a chau</button>
-            <h1 className='bg-red-500'>hola</h1>
-        </nav>
+        <div className='bg-color-blue'>
+            h
+        </div>
     )
 }
 
