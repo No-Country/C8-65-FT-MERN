e01c2a45e6a9a3b9615f24be8687113559fe48be
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-11-15 02:11:29 -0300

    NavBar

--- proyecto-farmacia/src/App.js
+++ proyecto-farmacia/src/App.js
@@ -1,30 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/screens/Home';
+import Cart from './components/screens/Cart';
 
 
 function App() {
   return (
-    <div >
+    <div>
       <NavBar />
-      <h1 className='text-red-600'>hola</h1>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          holaaa
-        </a>
-      </header>
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </div>
   );
 }
